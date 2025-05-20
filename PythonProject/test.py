36f1191c4bf17bb4ee7ffffaa9426a015ea960d0
SanH3x <heitorvicente_japa@outlook.com>
2025-05-20 20:43:10 -0300

    Descrição do arquivo stockprophet.py

--- PythonProject/test.py
+++ PythonProject/test.py
@@ -1,15 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-x= np.linspace(5,1,200)
-y= np.linspace(-5,5,100)
-X, Y= np.meshgrid(x,y)
-
-Z= np.sin(np.sqrt(-X**2 + Y**-2))
-plt.contour(X,Y,Z, levels=50, cmap='plasma')
-plt.colorbar(label='Heigh')
-plt.title('Contour Plot')
-plt.xlabel('Eixo X')
-plt.ylabel('Eixo Y')
-plt.show()
+"""
+Interface desenvolvida com Streamlit, usando modelo de séries temporais Prophet para prever preço das ações de empresas brasileiras.
+"""
 
